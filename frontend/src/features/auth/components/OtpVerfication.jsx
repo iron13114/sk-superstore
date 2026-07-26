73c05fc64d78f0a4e975fe1e8316bdab4390ba1a
Priyanshu <priyanshuprince2007@gmail.com>
2026-07-26 11:16:26 +0530

    added dependency array to the otp verification page

--- frontend/src/features/auth/components/OtpVerfication.jsx
+++ frontend/src/features/auth/components/OtpVerfication.jsx
@@ -1,119 +1,136 @@
-import {FormHelperText, Paper, Stack, TextField, Typography } from '@mui/material'
-import React, { useEffect} from 'react'
+import { FormHelperText, Paper, Stack, TextField, Typography, Button } from '@mui/material'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { clearOtpVerificationError, clearResendOtpError, clearResendOtpSuccessMessage, resendOtpAsync, resetOtpVerificationStatus, resetResendOtpStatus, selectLoggedInUser, selectOtpVerificationError, selectOtpVerificationStatus, selectResendOtpError, selectResendOtpStatus, selectResendOtpSuccessMessage, verifyOtpAsync } from '../AuthSlice'
+import { 
+  clearOtpVerificationError, 
+  clearResendOtpError, 
+  clearResendOtpSuccessMessage, 
+  resendOtpAsync, 
+  resetOtpVerificationStatus, 
+  resetResendOtpStatus, 
+  selectLoggedInUser, 
+  selectOtpVerificationError, 
+  selectOtpVerificationStatus, 
+  selectResendOtpError, 
+  selectResendOtpStatus, 
+  selectResendOtpSuccessMessage, 
+  verifyOtpAsync,
+  signOutAsync 
+} from '../AuthSlice'
 import { LoadingButton } from '@mui/lab'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form"
-import {toast} from 'react-toastify'
-
+import { toast } from 'react-toastify'
 
 export const OtpVerfication = () => {
-    
-    const {register,handleSubmit,formState: { errors }} = useForm()
-    const dispatch=useDispatch()
-    const loggedInUser=useSelector(selectLoggedInUser)
-    const navigate=useNavigate()
-    const resendOtpStatus=useSelector(selectResendOtpStatus)
-    const resendOtpError=useSelector(selectResendOtpError)
-    const resendOtpSuccessMessage=useSelector(selectResendOtpSuccessMessage)
-    const otpVerificationStatus=useSelector(selectOtpVerificationStatus)
-    const otpVerificationError=useSelector(selectOtpVerificationError)
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const dispatch = useDispatch()
+    const loggedInUser = useSelector(selectLoggedInUser)
+    const navigate = useNavigate()
+    const resendOtpStatus = useSelector(selectResendOtpStatus)
+    const resendOtpError = useSelector(selectResendOtpError)
+    const resendOtpSuccessMessage = useSelector(selectResendOtpSuccessMessage)
+    const otpVerificationStatus = useSelector(selectOtpVerificationStatus)
+    const otpVerificationError = useSelector(selectOtpVerificationError)
 
-    // handles the redirection
-    useEffect(()=>{
-        if(!loggedInUser){
+    useEffect(() => {
+        if (!loggedInUser) {
             navigate('/login')
-        }
-        else if(loggedInUser && loggedInUser?.isVerified){
+        } else if (loggedInUser && loggedInUser?.isVerified) {
             navigate("/")
         }
-    })
+    }, [loggedInUser, navigate])
 
-    const handleSendOtp=()=>{
-        const data={user:loggedInUser?._id}
+    const handleSendOtp = () => {
+        const data = { user: loggedInUser?._id }
         dispatch(resendOtpAsync(data))
     }
     
-    const handleVerifyOtp=(data)=>{
-        const cred={...data,userId:loggedInUser?._id}
+    const handleVerifyOtp = (data) => {
+        const cred = { ...data, userId: loggedInUser?._id }
         dispatch(verifyOtpAsync(cred))
     }
 
-    // handles resend otp error
-    useEffect(()=>{
-        if(resendOtpError){
+    const handleBackToLogin = () => {
+        dispatch(signOutAsync())
+        navigate('/login')
+    }
+
+    // Handles resend otp error
+    useEffect(() => {
+        if (resendOtpError) {
             toast.error(resendOtpError.message)
         }
-        return ()=>{
+        return () => {
             dispatch(clearResendOtpError())
         }
-    })
+    }, [resendOtpError, dispatch])
 
-    // handles resend otp success message
-    useEffect(()=>{
-        if(resendOtpSuccessMessage){
+    // Handles resend otp success message
+    useEffect(() => {
+        if (resendOtpSuccessMessage) {
             toast.success(resendOtpSuccessMessage.message)
         }
-        return ()=>{
+        return () => {
             dispatch(clearResendOtpSuccessMessage())
         }
-    })
+    }, [resendOtpSuccessMessage, dispatch])
 
-    // handles error while verifying otp
-    useEffect(()=>{
-        if(otpVerificationError){
+    // Handles error while verifying otp
+    useEffect(() => {
+        if (otpVerificationError) {
             toast.error(otpVerificationError.message)
         }
-        return ()=>{
+        return () => {
             dispatch(clearOtpVerificationError())
         }
-    })
+    }, [otpVerificationError, dispatch])
 
-    useEffect(()=>{
-        if(otpVerificationStatus==='fullfilled'){
+    useEffect(() => {
+        if (otpVerificationStatus === 'fulfilled' || otpVerificationStatus === 'fullfilled') {
             toast.success("Email verified! We are happy to have you here")
             dispatch(resetResendOtpStatus())
         }
-        return ()=>{
+        return () => {
             dispatch(resetOtpVerificationStatus())
         }
-    })
+    }, [otpVerificationStatus, dispatch])
 
-  return (
-    <Stack width={'100vw'} height={'100vh'} noValidate flexDirection={'column'} rowGap={3} justifyContent="center" alignItems="center" >
+    return (
+        <Stack width={'100vw'} height={'100vh'} flexDirection={'column'} rowGap={3} justifyContent="center" alignItems="center">
+            <Stack component={Paper} elevation={1} position={'relative'} justifyContent={'center'} alignItems={'center'} p={'2rem'} rowGap={'2rem'} width={'26rem'}>
+                
+                <Typography mt={2} variant='h5' fontWeight={500}>Verify Your Email Address</Typography>
 
-        
-        <Stack component={Paper} elevation={1} position={'relative'} justifyContent={'center'} alignItems={'center'} p={'2rem'} rowGap={'2rem'}>
-            
-            <Typography mt={4} variant='h5' fontWeight={500}>Verify Your Email Address</Typography>
-
-            {
-                resendOtpStatus==='fullfilled'?(
+                {resendOtpStatus === 'fulfilled' || resendOtpStatus === 'fullfilled' ? (
                     <Stack width={'100%'} rowGap={'1rem'} component={'form'} noValidate onSubmit={handleSubmit(handleVerifyOtp)}>
                         <Stack rowGap={'1rem'}> 
                             <Stack>
-                                <Typography  color={'GrayText'}>Enter the 4 digit OTP sent on</Typography>
+                                <Typography color={'GrayText'}>Enter the 4 digit OTP sent on</Typography>
                                 <Typography fontWeight={'600'} color={'GrayText'}>{loggedInUser?.email}</Typography>
                             </Stack>
                             <Stack>
-                                <TextField {...register("otp",{required:"OTP is required",minLength:{value:4,message:"Please enter a 4 digit OTP"}})} fullWidth type='number' />
-                                {errors?.otp && <FormHelperText sx={{color:"red"}}>{errors.otp.message}</FormHelperText>}
+                                <TextField {...register("otp", { required: "OTP is required", minLength: { value: 4, message: "Please enter a 4 digit OTP" } })} fullWidth type='number' />
+                                {errors?.otp && <FormHelperText sx={{ color: "red" }}>{errors.otp.message}</FormHelperText>}
                             </Stack>
-                       </Stack>
-                        <LoadingButton loading={otpVerificationStatus==='pending'}  type='submit' fullWidth variant='contained'>Verify</LoadingButton>
+                        </Stack>
+                        <LoadingButton loading={otpVerificationStatus === 'pending'} type='submit' fullWidth variant='contained'>Verify</LoadingButton>
                     </Stack>
-                ):
-                <>
-                <Stack>
-                    <Typography color={'GrayText'}>We will send you a OTP on</Typography>
-                    <Typography fontWeight={'600'} color={'GrayText'}>{loggedInUser?.email}</Typography>
-                </Stack>
-                <LoadingButton onClick={handleSendOtp} loading={resendOtpStatus==='pending'} fullWidth variant='contained'>Get OTP</LoadingButton>
-                </>
-             }
+                ) : (
+                    <Stack width={'100%'} rowGap={'1.5rem'} alignItems={'center'}>
+                        <Stack textAlign={'center'}>
+                            <Typography color={'GrayText'}>We will send an OTP to</Typography>
+                            <Typography fontWeight={'600'} color={'GrayText'}>{loggedInUser?.email || "your registered email"}</Typography>
+                        </Stack>
+                        <LoadingButton onClick={handleSendOtp} loading={resendOtpStatus === 'pending'} fullWidth variant='contained'>Get OTP</LoadingButton>
+                    </Stack>
+                )}
 
+                <Button variant="text" size="small" onClick={handleBackToLogin} sx={{ mt: 1 }}>
+                    ← Back to Login
+                </Button>
+
+            </Stack>
         </Stack>
-    </Stack>
-  )
+    )
 }

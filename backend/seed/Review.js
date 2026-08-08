dce3beed02ec29ca47cf387c6a69503a7448d00e
Priyanshu <priyanshuprince2007@gmail.com>
2026-08-08 22:42:52 +0530

    fixed the problem with addition of diffrent product in cart not showing in the cart page and review issue with not logged in users

--- backend/seed/Review.js
+++ backend/seed/Review.js
@@ -7,17 +7,17 @@ const reviews = [
     product: "65c357fe2f21c40d167c27a1",
     rating: 5,
     comment:
-      "Exceeded expectations! This phone is a game-changer. Lightning fast, stunning camera, incredible battery life. Best phone ever! ",
-    createdAt: "2024-02-07T10:25:58.424Z",
+      "Exceeded expectations! These Lays chips are a game-changer. Perfectly crispy, amazing flavors, incredible crunch. Best chips ever!",
+    createdAt: "2026-07-30T10:01:00.000Z"
   },
   {
     _id: "65c25339dcd9253acfbaa79e",
     user: "65c2526fdcd9253acfbaa731",
     product: "65c357fe2f21c40d167c27a2",
-    rating: 3,
+    rating: 5,
     comment:
-      "Good, not mind-blowing. Decent phone, not revolutionary. Average camera, battery life, performance.",
-    createdAt: "2024-02-07T10:25:58.424Z",
+      "Good, not mind-blowing. Decent detergent, not revolutionary. Cleans clothes well, but the fragrance and stain removal are average.",
+    createdAt: "2026-07-30T11:01:00.000Z",
   },
 ];
 
